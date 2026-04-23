be54ab210d9b313fdc6af69f1ffc0dd550dbbe63
Natsugure <natsugure5301102@gmail.com>
2026-04-24 07:57:48 +0900

    不要なimportを削除

--- src/tests/component.spec.tsx
+++ src/tests/component.spec.tsx
@@ -1,5 +1,4 @@
 import App from "../App";
-import React from "react";
 import { describe, it, expect } from "vitest";
 import { render, screen, waitFor } from "@testing-library/react";
 import "@testing-library/jest-dom/vitest";
