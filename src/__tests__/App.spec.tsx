47688c72158a412deb9718643f8ed386b566ff44
Natsugure <natsugure5301102@gmail.com>
2026-04-27 08:02:44 +0900

    タイトルとボタン表示のテストを追加

--- src/__tests__/App.spec.tsx
+++ src/__tests__/App.spec.tsx
@@ -3,11 +3,18 @@ import { describe, it, expect } from "vitest";
 import { render, screen, waitFor } from "@testing-library/react";
 import "@testing-library/jest-dom/vitest";
 
-describe("Title Test", () => {
+describe("App.tsx Test", () => {
   it("タイトルが学習記録アプリであること", async () => {
     render(<App />);
 
     await waitFor(() => expect(screen.queryByTestId("loading-text")).not.toBeInTheDocument());
     expect(screen.getByTestId("title")).toHaveTextContent("学習記録アプリ");
+  });
+
+  it("「追加」ボタンが表示されること", async () => {
+    render(<App />);
+
+    await waitFor(() => expect(screen.queryByTestId("loading-text")).not.toBeInTheDocument());
+    expect(screen.getByRole("button", { name: "追加" })).toBeInTheDocument();
   });
 });
